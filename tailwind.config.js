7c04585ea44204d62699bcef7e946c45d0fe0bf4
Benjamin Cloquet <benjamin.cloquet@gmail.com>
2025-06-13 12:28:30 +0200

    fix: rework design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
-  theme: {
-    extend: {
-      colors: {
-        "orange": "#ffa533"
-      },
-    },
-  },
   plugins: [],
-}
+};
